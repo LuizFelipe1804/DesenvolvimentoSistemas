db511e6083d389bbe968c75785ff6910d44e72aa
LuizFelipe1804 <luizfelipearaujosv1804@gmail.com>
2025-02-21 14:07:56 +0000

    cauculadora do satanas

---
+++ aula00/aula01/cauculadora.js
@@ -0,0 +1,8 @@
+let expressao = "5 + 8.5 * 2"
+let resultado = eval(expressao)
+
+console.log(resultado);
+
+
+
+
--- aula00/aula01/questao41.js
+++ aula00/aula01/questao41.js
@@ -3,8 +3,8 @@ let engordar = peso * 1.15;
 let emagrecer = peso * 0.80; 
 
 
-console.log("peso atual:"+ peso);
-console.log(engorda);
+console.log("peso atual:");
+console.log(engorda );
 console.log(emagrecer);
 
 
