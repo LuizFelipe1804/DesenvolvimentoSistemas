f0683f2e26b9ab1c6d07585b3fb7283e6cae6d6e
LuizFelipe1804 <luizfelipearaujosv1804@gmail.com>
2025-03-21 16:50:35 +0000

    questao2

--- aula00/Atividade01-TiposDados/questao01.js
+++ aula00/Atividade01-TiposDados/questao01.js
@@ -4,10 +4,11 @@ let n3 = 3
 let n4 = 2
 let n5 = 1
 
-let ext1 = Aprendi
-let ext2 = JavaScript
+let apr = Aprendi
+let js = JavaScript
 
- console.log(n5,","n4",",n3,"",n2,"",n1,"...",ext1,"",ext2);
+ console.log( n5 , n4 , n3 ,n2 , n1 , apr , js );
+
 
  
  
---
+++ aula00/Atividade01-TiposDados/questao02.js
@@ -0,0 +1,36 @@
+let n1 = 0
+let n2 = 3.14
+let nome = Infinity
+let nome2 = NaN
+let verdade = true
+let n3 = 4>1
+let txt = "texto"
+let txt2 = "é tetra"+1994
+let symbol = ("id")
+let Data = Date
+let txt3 = String
+let n4 = [1,2,3,4]
+let txt4 = null
+let txt5 = undefined
+
+
+console.log(typeof(n1));
+console.log(typeof(n2));
+console.log(typeof(nome));
+console.log(typeof(nome2));
+console.log(typeof(verdade));
+console.log(typeof(n3));
+console.log(typeof(txt));
+console.log(typeof(txt2));
+console.log(typeof(Symbol));
+console.log(typeof(Date));
+console.log(typeof(txt3));
+console.log(typeof(n4));
+console.log(typeof(txt4));
+console.log(typeof(txt5));
+
+
+
+
+
+
