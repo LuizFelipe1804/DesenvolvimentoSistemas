0512a70171c0edc0ef68732fe206d5ca77f48b27
LuizFelipe1804 <luizfelipearaujosv1804@gmail.com>
2025-03-21 16:48:35 +0000

    atividade

---
+++ aula00/Atividade01-TiposDados/questao01.js
@@ -0,0 +1,16 @@
+let n1 = 5
+let n2 = 4
+let n3 = 3
+let n4 = 2
+let n5 = 1
+
+let ext1 = Aprendi
+let ext2 = JavaScript
+
+ console.log(n5,","n4",",n3,"",n2,"",n1,"...",ext1,"",ext2);
+
+ 
+ 
+
+
+
